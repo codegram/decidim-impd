366870b728b776b58ba65709cec05b4f37ef2e0b
Oriol Gual <oriol@codegram.com>
2021-03-19 09:40:10 +0100

    Configure letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
   mount Decidim::Core::Engine => '/'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
