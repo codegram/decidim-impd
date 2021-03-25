b487e1bf8494b3f4d4003dd0e5e58f453f6a34db
Oriol Gual <oriol@codegram.com>
2021-03-25 21:01:52 +0100

    Update wording and change required fields

--- decidim-module-elections_census/app/forms/decidim/elections_census/voter_form.rb
+++ decidim-module-elections_census/app/forms/decidim/elections_census/voter_form.rb
@@ -7,7 +7,7 @@ module Decidim
 
       DOCUMENT_TYPES = %w(dni nie passport)
       DISABILITIES = %w(physical mental_disorder intellectual auditory_sensory visual_sensory)
-      GENDERS = %w(female male other)
+      GENDERS = %w(female male)
 
       attribute :document_type, String
       attribute :document_number, String
@@ -35,7 +35,7 @@ module Decidim
       attribute :legal_guardian_lastname, String
       attribute :legal_guardian_second_lastname, String
 
-      validates :document_type, :name, :lastname, :disability, :address, :birthday, :address_type, :address_name, :address_number, :address_floor, :address_door, :address_postal_code, :disability_certificate, presence: true
+      validates :document_type, :name, :lastname, :disability, :address, :birthday, :address_type, :address_name, :address_number, :address_floor, :address_door, :address_postal_code, :disability_certificate, :email, :mobile_phone_number, presence: true
       validates :tos_agreement, acceptance: true
       validates :document_type, inclusion: { in: DOCUMENT_TYPES }
       validates :disability, inclusion: { in: DISABILITIES }
@@ -45,7 +45,6 @@ module Decidim
       validates :family_book, :legal_guardian_document_number, :legal_guardian_name, :legal_guardian_lastname, :legal_guardian_second_lastname, presence: true, if: ->(form) { form.needs_legal_guardian? }
       validates :legal_guardian_document_type, inclusion: { in: DOCUMENT_TYPES }, if: ->(form) { form.needs_legal_guardian? }
       validate :different_disabilities
-      validate :email_or_phone
       validate :exists_in_census
 
       def self.from_params(params, additional_params = {})
@@ -75,13 +74,6 @@ module Decidim
       end
 
       private
-
-      def email_or_phone
-        return if email.present? || mobile_phone_number.present?
-
-        errors.add(:email, :email_or_phone_required)
-        errors.add(:mobile_phone_number, :email_or_phone_required)
-      end
 
       def different_disabilities
         return if disability != secondary_disability
