6266a35be1db013bc27a1d6ac1b9b06058545a2c
Oriol Gual <oriol@codegram.com>
2021-05-23 13:18:56 +0200

    Voting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,17 @@ if ENV["HEROKU_APP_NAME"].present?
   ENV["SEED"] = "true"
 end
 
-require "decidim/faker/localized"
+return if Rails.env.production? && Decidim::Organization.count > 0
 
-Decidim::Organization.first || Decidim::Organization.create!(
-  name: Faker::Company.name,
-  twitter_handler: Faker::Hipster.word,
-  facebook_handler: Faker::Hipster.word,
-  instagram_handler: Faker::Hipster.word,
-  youtube_handler: Faker::Hipster.word,
-  github_handler: Faker::Hipster.word,
+if Rails.env.development?
+  Decidim::ElectionsCensus::Voter.delete_all
+  Decidim::ElectionsCensus::Vote.delete_all
+end
+
+I18n.locale = :ca
+
+organization = Decidim::Organization.first || Decidim::Organization.create!(
+  name: "Staging IMPD",
   smtp_settings: {
     from: ENV["EMAIL"],
     user_name: ENV["SENDGRID_USERNAME"],
@@ -30,12 +32,13 @@ Decidim::Organization.first || Decidim::Organization.create!(
     enable_starttls_auto: true
   },
   host: ENV["DECIDIM_HOST"] || "localhost",
-  description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-    Decidim::Faker::Localized.sentence(15)
-  end,
-  default_locale: Decidim.default_locale,
-  available_locales: Decidim.available_locales,
-  reference_prefix: Faker::Name.suffix,
+  description: {
+    "ca" => "Entorn de proves per l'IMPD",
+    "es" => "Entorno de pruebas para el IMPD",
+  },
+  default_locale: :ca,
+  available_locales: [:ca, :es],
+  reference_prefix: "sg-impd",
   available_authorizations: Decidim.authorization_workflows.map(&:name),
   users_registration_mode: :enabled,
   tos_version: Time.current,
@@ -44,4 +47,57 @@ Decidim::Organization.first || Decidim::Organization.create!(
   send_welcome_notification: true
 )
 
-Decidim.seed!
+def create_voter(params)
+  Decidim::ElectionsCensus::Voter.create!(
+    name: "Votant",
+    lastname: [translate_disability(params["disability"]), translate_disability(params["secondary_disability"])].reject(&:blank?).join(" - "),
+    disability: params["disability"],
+    secondary_disability: params["secondary_disability"],
+    birthday: Date.new(1980, 1, 1),
+    verified_at: Time.current,
+    document_type: "dni",
+    document_number: params["document_number"],
+    organization: params["organization"]
+  )
+end
+
+def translate_disability(value)
+  return value if value.blank?
+  I18n.t(value, scope: "decidim.elections_census.disabilities")
+end
+
+disabilities = Decidim::ElectionsCensus::Vote::CANDIDATES.keys
+counter = 0
+
+disabilities.each do |disability|
+  10.times.each do |index|
+    counter += 1
+    params = {
+      "disability" => disability,
+      "document_number" => counter.to_s.rjust(9, "0"),
+        "organization" => organization
+    }
+
+    create_voter(params)
+  end
+
+  other_disabilities = disabilities - [disability]
+  other_disabilities.each do |secondary_disability|
+    5.times.each do |index|
+      counter += 1
+      params = {
+        "disability" => disability,
+        "secondary_disability" => secondary_disability,
+        "document_number" => counter.to_s.rjust(9, "0"),
+        "organization" => organization
+      }
+
+      create_voter(params)
+    end
+  end
+end
+
+puts ["Tipus de document", "Número de document", "Discapacitat", "Discapacitat secundària", "Data de naixement"].join(", ")
+Decidim::ElectionsCensus::Voter.all.each do |voter|
+  puts [voter.document_type.upcase, voter.document_number, translate_disability(voter.disability), translate_disability(voter.secondary_disability), voter.birthday.strftime("%d-%m-%Y")].join(", ")
+end
