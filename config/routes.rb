2bae44a527d605a79fde6ec06a6ecaaa9aa275ce
Oriol Gual <oriol@codegram.com>
2021-05-26 17:00:36 +0200

    Add quick access urls

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get '/elections-census/:path(*all)' => redirect('/elections/%{path}%{all}')
   get '/elections-census' => redirect('/elections')
+  get '/vota' => redirect('/elections/booth/auth')
+  get '/vota-es' => redirect('/elections/booth/auth?locale=es')
+  get '/comprova-vot' => redirect('/elections/verify')
+  get '/comprueba-voto' => redirect('/elections/verify?locale=es')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
