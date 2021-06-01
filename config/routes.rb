2191effc45fe0d94e42e88db17cc966b3f75f5c1
Oriol Gual <oriol@codegram.com>
2021-06-01 08:36:36 +0200

    Fix redirection

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   get '/elections-census/:path(*all)' => redirect('/elections/%{path}%{all}')
+  get '/elections-census/:path' => redirect('/elections/%{path}')
   get '/elections-census' => redirect('/elections')
   get '/vota' => redirect('/elections/booth/auth?locale=ca')
   get '/vota-es' => redirect('/elections/booth/auth?locale=es')
   get '/comprova-vot' => redirect('/elections/verify?locale=ca')
   get '/comprueba-voto' => redirect('/elections/verify?locale=es')
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
